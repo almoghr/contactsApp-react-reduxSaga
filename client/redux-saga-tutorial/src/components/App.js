f1944f2d6987d5b09f3ee39df1cbb5f6705d0b31
almog ram <almoghr36@gmail.com>
2021-02-04 13:24:19 +0200

    added error handling

--- client/redux-saga-tutorial/src/components/App.js
+++ client/redux-saga-tutorial/src/components/App.js
@@ -1,37 +1,59 @@
-import {Component} from 'react';
-import { connect } from 'react-redux';
-import {getUsersRequest, createUserRequest, deleteUserRequest} from '../actions/users';
-import UsersList from './UsersList';
-import NewUserForm from './NewUserForm';
+import { Component } from "react";
+import { connect } from "react-redux";
+import {
+  getUsersRequest,
+  createUserRequest,
+  deleteUserRequest,
+  usersError,
+} from "../actions/users";
+import UsersList from "./UsersList";
+import NewUserForm from "./NewUserForm";
+import { Alert } from "reactstrap";
+ 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.props.getUsersRequest();
-  };
-
-  handleSubmit = ({firstName, lastName}) => {
-    this.props.createUserRequest({firstName, lastName});
   }
+
+  handleSubmit = ({ firstName, lastName }) => {
+    this.props.createUserRequest({ firstName, lastName });
+  };
 
   handleDeleteUserClick = (userId) => {
     this.props.deleteUserRequest(userId);
-  }
+  };
+
+  handleCloseAlert = () => {
+    this.props.usersError({ 
+      error: ''
+    })
+  };
   render() {
     const users = this.props.users;
-    return(
-      <div style={{margin: '0 auto', padding: '20px', maxWidth: '600px'}}> 
+    return (
+      <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
+        <Alert
+          color="danger"
+          isOpen={!!this.props.users.error}
+          toggle={this.handleCloseAlert}
+        >
+          {this.props.users.error}
+        </Alert>
         <NewUserForm onSubmit={this.handleSubmit} />
-        <UsersList onDeleteUser={this.handleDeleteUserClick} users={users.items} />
+        <UsersList
+          onDeleteUser={this.handleDeleteUserClick}
+          users={users.items}
+        />
       </div>
-    )
+    );
   }
 }
 
-
-
-export default connect(({users}) => ({users}), { 
+export default connect(({ users }) => ({ users }), {
   getUsersRequest,
-  createUserRequest, 
-  deleteUserRequest
+  createUserRequest,
+  deleteUserRequest,
+  usersError
 })(App);
--- client/redux-saga-tutorial/src/sagas/users.js
+++ client/redux-saga-tutorial/src/sagas/users.js
@@ -1,52 +1,77 @@
-import {takeEvery, takeLatest, take, call, fork, put} from 'redux-saga/effects';
-import * as actions from '../actions/users'
-import * as api from '../api/users'
+import {
+  takeEvery,
+  takeLatest,
+  take,
+  call,
+  fork,
+  put,
+} from "redux-saga/effects";
+import * as actions from "../actions/users";
+import * as api from "../api/users";
 
 function* getUsers() {
-    try{
-        const result = yield call(api.getUsers);
-        yield put(actions.getUsersSuccess({
-            items:result.data.data
-        }));
-    } catch(e){
+  try {
+    const result = yield call(api.getUsers);
+    yield put(
+      actions.getUsersSuccess({
+        items: result.data.data,
+      })
+    );
+  } catch (e) {
+    yield put(
+        actions.usersError({
+          error: "An error occured while trying to get users",
+        })
+      );
+  
+  }
+}
+function* watchGetUsersRequest() {
+  yield takeEvery(actions.Types.GET_USERS_REQUEST, getUsers);
+}
 
-    }
+function* createUser(action) {
+  try {
+    yield call(api.createUser, {
+      firstName: action.payload.firstName,
+      lastName: action.payload.lastName,
+    });
+    yield call(getUsers);
+  } catch (e) {
+    yield put(
+        actions.usersError({
+          error: "An error occured while trying to create a user",
+        })
+      );
+  
+  }
 }
-function* watchGetUsersRequest(){
-    yield takeEvery(actions.Types.GET_USERS_REQUEST, getUsers)
+function* watchCreateUserRequest() {
+  yield takeLatest(actions.Types.CREATE_USER_REQUEST, createUser);
+}
+function* deleteUser({ userId }) {
+  try {
+    yield call(api.deleteUser, userId);
+  } catch (e) {
+    yield put(
+      actions.usersError({
+        error: "An error occured while trying to delete a user",
+      })
+    );
+  }
+}
+function* watchDeleteUserRequest() {
+  while (true) {
+    const action = yield take(actions.Types.DELETE_USER_REQUEST);
+    yield call(deleteUser, { userId: action.payload.userId });
+    yield call(getUsers);
+  }
 }
 
-function* createUser(action){
-    try{
-        yield call(api.createUser, {firstName: action.payload.firstName, lastName: action.payload.lastName});
-        console.log('works')
-            yield call(getUsers)
-        console.log('works2')
-    } catch (e){
-        
-    }
-}
-function* watchCreateUserRequest(){
-    yield takeLatest(actions.Types.CREATE_USER_REQUEST, createUser)
-}
-function* deleteUser({userId}){
-    try{
-        yield call(api.deleteUser, userId)
-    } catch (e){
-        
-    }
-}
-function* watchDeleteUserRequest(){
-    while(true) {
-        const action = yield take(actions.Types.DELETE_USER_REQUEST);
-        yield call(deleteUser, {userId: action.payload.userId});
-        yield call(getUsers)
-    }
-}
 const usersSagas = [
-    fork(watchGetUsersRequest),
-    fork(watchCreateUserRequest),
-    fork(watchDeleteUserRequest),
+  fork(watchGetUsersRequest),
+  fork(watchCreateUserRequest),
+  fork(watchDeleteUserRequest),
 ];
 
-export default usersSagas
+export default usersSagas;
